bd0fee856c24c3557bdc4ac297993b7b9c4ca6a1
Ray Lara <raymundolarasandoval@gmail.com>
2025-09-02 23:22:34 -0600

    cambio bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = window.location.origin; // Usará https:// con la URL pública actual
-axios.defaults.withCredentials = true; // Si usas sesión/cookies
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
+axios.defaults.withCredentials = false;
 
 export default axios;
 
